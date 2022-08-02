c423f99b828bd862c048c1ca7f33486e7f1a41eb
Seyoon Ko <kos@ucla.edu>
2022-08-01 22:33:33 -0700

    update config

--- Cluster/startup.jl
+++
@@ -1,9 +0,0 @@
-using Pluto, Pkg
-notebookdir = "JSM-Julia-Short-Course-2022/notebooks/"
-filelist = readdir(notebookdir)
-for file in filelist
-    if endswith(file, ".jl")
-        Pluto.activate_notebook_environment(notebookdir * file)
-        Pkg.instantiate()
-    end 
-end
